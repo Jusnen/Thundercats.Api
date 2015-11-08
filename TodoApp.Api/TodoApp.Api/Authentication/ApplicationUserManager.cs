5be8af2ab805a1f75209dd3ceeae97c966dff764
Jusnen <jusnenvolquez@gmail.com>
2015-11-08 19:47:36 -0400

    Add TodoDbContext
    Add ApplicationUserManager

---
+++ TodoApp.Api/TodoApp.Api/Authentication/ApplicationUserManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Api.Db;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Authentication
+{
+    public class ApplicationUserManager : UserManager<Owner>
+    {
+        public ApplicationUserManager(IUserStore<Owner> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var appDbContext = context.Get<TodoDbContext>();
+            var appUserManager = new ApplicationUserManager(new UserStore<Owner>(appDbContext));
+
+            return appUserManager;
+        }
+    }
+}
---
+++ TodoApp.Api/TodoApp.Api/Db/TodoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Db
+{
+    public class TodoDbContext : IdentityDbContext
+    {
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Assistant> Assistants { get; set; }
+        public DbSet<Home> Homes { get; set; }
+
+        public TodoDbContext() : base("TodoDb")
+        {
+
+        }
+        public static TodoDbContext Create()
+        {
+            return new TodoDbContext();
+        }
+ 
+    }
+}
